17df2be3d6e21e758d3fb44640ac3836606e5c5b
drozdziel.m@gmail.com <drozdziel.m@gmail.com@03d28ff4-47f1-384a-5b41-cf837e6a7daa>
2012-07-12 20:00:02 +0000

    NetBeans Project: -SD.java : works just like the C version -DBConnection.java: connection with SQL database
    TODO:
    -hardwareConnection.java: connection with sensors and actuators
    -BMS.java: main wrapper class

--- BMS/trunk/BMS/src/bms/SD.java
+++ BMS/trunk/BMS/src/bms/SD.java
@@ -4,15 +4,18 @@
  */
 package bms;
 
+import java.util.TimerTask;
+
 /**
  *
  * @author Michal
  */
-public class SD {
+public class SD extends TimerTask {
 
     static int N = 3;
+    static float t = 15;
 
-    private float gaussian(double b, double c, double x) {
+    private static float gaussian(double b, double c, double x) {
         double a;
         float value;
         a = 1 / (c * Math.sqrt(2 * Math.PI));
@@ -21,7 +24,7 @@ public class SD {
         return value;
     }
 
-    private float fuzzy_S(float wartosc, float p1, float p2) { //S
+    private static float fuzzy_S(float wartosc, float p1, float p2) { //S
         float wynik;
         if (wartosc <= p1) {
             wynik = 0;
@@ -33,7 +36,7 @@ public class SD {
         return wynik;
     }
 
-    private float fuzzy_Z(float wartosc, float p1, float p2) { //Z
+    private static float fuzzy_Z(float wartosc, float p1, float p2) { //Z
         float wynik;
         if (wartosc <= p1) {
             wynik = 1;
@@ -45,7 +48,7 @@ public class SD {
         return wynik;
     }
 
-    private float fuzzy_trojkat(float wartosc, float p1, float p2, float p3) { //trojkatna funkcja
+    private static float fuzzy_trojkat(float wartosc, float p1, float p2, float p3) { //trojkatna funkcja
         float wynik;
         if (wartosc <= p1 || wartosc >= p3) {
             wynik = 0;
@@ -59,7 +62,7 @@ public class SD {
         return wynik;
     }
 
-    private float fuzzy_trapez(float wartosc, float p1, float p2, float p3, float p4) { //trapezowa funkcja
+    private static float fuzzy_trapez(float wartosc, float p1, float p2, float p3, float p4) { //trapezowa funkcja
         float wynik;
         if (wartosc <= p1 || wartosc >= p4) {
             wynik = 0;
@@ -73,44 +76,39 @@ public class SD {
         return wynik;
     }
 
-    private float fuzzyAND(float f1, float f2) {
+    private static float fuzzyAND(float f1, float f2) {
         if (f1 > f2) {
             return f2;
         }
         return f1;
     }
 
-    private float fuzzyOR(float f1, float f2) {
+    private static float fuzzyOR(float f1, float f2) {
         if (f1 > f2) {
             return f1;
         }
         return f2;
     }
 
-    private float fuzzyNOT(float f1) {
+    private static float fuzzyNOT(float f1) {
         return 1 - f1;
     }
 
-    Regula[] odczyt_z_bazy(int czujnik) {
-        //TODO 
-        Regula[] r = new Regula[N];
-        r[0] = new Regula(1, 0, 19, 0);
-        r[1] = new Regula(1, 0, 20, 0);
-        r[2] = new Regula(1, 0, 21, 0);
-        return r;
+    private static Regula[] odczyt_z_bazy(int czujnik) {
+        return DBConnection.Select_All_From_Reguly("id_czuj=" + String.valueOf(czujnik));
     }
 
-    private float oblicz(Regula[] r, float f) {
+    private static float oblicz(Regula[] r, float f) {
         float zz, z, i, c, zc, val[];
         float value = 0;
-        int j = 0;
+
         val = new float[2];//0-grzej, 1-wietrz
-        for (j = 0; j < r.length; ++j) {
+        for (int j = 0; j < r.length; ++j) {
             switch (r[j].getFunkcja()) {
                 case 0:
                     zz = gaussian(0, 10, f);
                     z = gaussian(r[j].getWartosc() - 5, 5, f);
-                    i = gaussian(r[j].getWartosc(), 1, f) / 0.4f;
+                    i = gaussian(r[j].getWartosc(), 1, f);
                     c = gaussian(r[j].getWartosc() + 5, 5, f);
                     zc = gaussian(40, 10, f);
                     val[0] = fuzzyOR(zz, z);//za zimno OR zimno-> grzej
@@ -135,22 +133,26 @@ public class SD {
         return value;
     }
 
-    private boolean ustaw(int obj, float val) {
+    private static boolean ustaw(int obj, float val) {
 
 
         return false;
     }
 
+    @Override
     public void run() {
         Regula[] tmp;
-        float tmp_f, t;
-        for (int i = 0; i < 21; i++) {
+        float tmp_f;
+        int czuj = DBConnection.Select_All_From_Czujniki();
+        for (int i = 1; i <= czuj; i++) {
             tmp = odczyt_z_bazy(i);
-            t = i * 0.5f + 15;
             tmp_f = oblicz(tmp, t);
             System.out.println(String.valueOf(t) + ": " + String.valueOf(tmp_f));
             ustaw(i, tmp_f);
         }
-
+        t += 0.5f;
+        if (t > 30) {
+            t = 10;
+        }
     }
 }
